994f767ccc581e177b2cd86fa6e232a5616fff8b
maxrabin <max.rabin17@gmail.com>
2021-07-09 10:45:53 -0400

    commit tree 7fbdf138000518d5c3cdb634bcff88b811c4e885

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, alive) VALUES ("Mr. Chocolate", 10, "M", "Brown", true);
+INSERT INTO bears (name, age, sex, color, alive) VALUES ("Rowdy", 5, "M", "Red", true);
+INSERT INTO bears (name, age, sex, color, alive) VALUES ("Tabitha", 15, "F", "Blue", true);
+INSERT INTO bears (name, age, sex, color, alive) VALUES ("Sergeant Brown", 7, "M", "Brown", true);
+INSERT INTO bears (name, age, sex, color, alive) VALUES ("Melissa", 11, "F", "Black", true);
+INSERT INTO bears (name, age, sex, color, alive) VALUES ("Grinch", 75, "M", "Green", true);
+INSERT INTO bears (name, age, sex, color, alive) VALUES ("Wendy", 9, "F", "Brown", true);
+INSERT INTO bears (name, age, sex, color, alive) VALUES (NULL, 7, "F", "Black", true);
